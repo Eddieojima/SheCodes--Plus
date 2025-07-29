a8436ffe1389f2207630ea63afc95cee70fba3bb
TrixieOjima <edwinblessing007@gmail.com>
2025-07-29 14:46:35 +0100

    added more weather data

--- week 7/index.js
+++ week 7/index.js
@@ -2,15 +2,41 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelectorT("#description");
+  let humidityElement = document.querySelectorT("#humidity");
+  let windSpeedElement = document.querySelectorT("#wind-speed");
+  let timeElement = document.querySelectorT("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = "$(response.data.temperature.humidity)%";
+  humidityElement.innerHTML = "$(response.data.wind.speed)km/h";
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return "${day} ${hours}:$(minutes)";
+}
+
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "baf09acda301ab227736t39o31e543b8";
   let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+    "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
   axios.get(apiUrl).then(refreshWeather);
 }
 
